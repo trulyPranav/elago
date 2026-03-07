9385bd7c6ee812ce404643ffe7e778855b3c546f
Pranav M <pranavm265@gmail.com>
2026-03-07 18:51:59 +0530

    configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
   },
-  // Ensure Leaflet CSS is bundled correctly
-  webpack: (config) => {
-    return config;
-  },
+  turbopack: {},
 }
 module.exports = nextConfig
