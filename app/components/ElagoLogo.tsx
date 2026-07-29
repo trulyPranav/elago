60e93bd1665baa51d9ce424a6869506c4f38ff44
Pranav M <pranavm265@gmail.com>
2026-07-29 11:08:10 +0530

    Fix

--- app/components/ElagoLogo.tsx
+++ app/components/ElagoLogo.tsx
@@ -13,7 +13,7 @@ export default function ElagoLogo({ className = '', size = 'md' }: ElagoLogoProp
 
   return (
     <Image
-      src="/elago_logo.png"
+      src="/elago-logo.png"
       alt="elaGO"
       width={width}
       height={height}
